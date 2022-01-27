f5d96330af2e47a01c012710e1aeb1ccf2f58cf1
jmcole9 <97640992+jmcole9@users.noreply.github.com>
2022-01-26 17:05:18 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,5 +1,11 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
 import org.junit.*;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class MarkdownParseTest {
     @Test
@@ -8,7 +14,23 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void testFile() {
-        assertEquals("[https://something.com, some-page.html]", MarkdownParse.getLinks("test-file.md"));
+    public void break1() throws IOException{
+        Path fileName = Path.of("test-break-1.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void break2() throws IOException{
+        Path fileName = Path.of("test-break-2.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("https://something().com"), MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void break3() throws IOException{
+        Path fileName = Path.of("test-break-3.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of(), MarkdownParse.getLinks(contents));
     }
 }
