dadc045bf7b79575364af61b6a1132748b801251
jmcole9 <97640992+jmcole9@users.noreply.github.com>
2022-02-09 15:45:35 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -54,6 +54,8 @@ public class MarkdownParse {
         }
         return toReturn;
     }
+
+    
     public static void main(String[] args) throws IOException {
 		Path fileName = Path.of(args[0]);
 	    String contents = Files.readString(fileName);
